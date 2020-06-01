d33a3fcd4d1ab77d65479d55d1dfb0a6a9711374
nganvo2 <hharuhhura@gmail.com>
2020-06-01 23:26:30 +0700

    update

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './react-native-src/navigation/AppNavigator';
-import { AppRoute } from './react-native-src/navigation/app-routes';
 
-const isAuthorized = true;
+function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
 
-export default class App extends React.Component {
-
-  render() {
-    return (
-
-      <NavigationContainer>
-        <AppNavigator initialRouteName={ AppRoute.HOME } />
-      </NavigationContainer>
-
-    );
-  }
-};
+export default App;
--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,14 @@
 // module.exports = {
-//     dependencies: {
-//         'react-native-live-stream': {
-//             platforms: {
-//                 ios: null
-//             },
-//         },
-//         'react-native-yz-vlcplayer': {
-//             platforms: {
-//                 ios: null
-//             },
-//         },
+//   dependencies: {
+//     // 'react-native-live-stream': {
+//     //   platforms: {
+//     //     ios: null,
+//     //   },
+//     // },
+//     'react-native-yz-vlcplayer': {
+//       platforms: {
+//         ios: null,
+//       },
 //     },
+//   },
 // };

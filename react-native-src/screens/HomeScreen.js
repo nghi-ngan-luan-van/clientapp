c030210825cea72b814843700545a1a68c63d06b
nganvo2 <hharuhhura@gmail.com>
2020-06-09 13:57:59 +0700

    update media

--- react-native-src/screens/HomeScreen.js
+++ react-native-src/screens/HomeScreen.js
@@ -173,7 +173,7 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
-        elevation: 5,
+        // elevation: 5,
     },
     iconAdd: {
         position: 'absolute',
--- react-native-src/screens/Media.js
+++ react-native-src/screens/Media.js
@@ -1,60 +1,28 @@
-import React, {useEffect, useState} from 'react';
-import {Dimensions, StyleSheet, TouchableOpacity, FlatList, Text, View, Image, Button, ScrollView} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Button, Dimensions, StyleSheet, Text, View} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import _ from "lodash";
 import {HOST_URL} from "../utils/AppConst";
 import VLCPlayer from 'react-native-vlc-media-player/VLCPlayer';
 import Timeline from "react-native-beautiful-timeline";
+import moment from "moment";
+
 const WIDTH = Dimensions.get('window').width;
 export default function Media(props, route) {
-    const url='https://clientapp.sgp1.digitaloceanspaces.com/5ed3e22848d6943ed70ec47f/5_5/_002.mp4'
+    const url = 'https://clientapp.sgp1.digitaloceanspaces.com/5ed3e22848d6943ed70ec47f/5_5/_002.mp4'
 
     const [camera, setCamera] = useState(_.get(props, 'route.params.camera', {}));
     const [cameras, setCameras] = useState(_.get(props, 'route.params.cameras', {}));
-    const [listVideo, setListVideo] = useState( []);
-    const [video, setVideo] = useState( url);
+    const [listVideo, setListVideo] = useState([]);
+    const [listEvent, setListEvent] = useState([]);
+    const [video, setVideo] = useState(url);
     const {_id = ''} = camera || {}
-    const data=[
-        {
-            "date": 1574342522000,
-            "data": [
-                {
-                    "title": "React Native Beautiful Timeline",
-                    "subtitle": "Sed at justo eros. Phasellus.",
-                    "date": Number(listVideo[0].timeStart)
-                },
-                {
-                    "title": "React Native",
-                    "subtitle": "Sed viverra. Nam sagittis.",
-                    "date": Number(listVideo[0].timeStart)
-                }
-            ]
-        },
-        {
-            "date": 1574248261000,
-            "data": [
-                {
-                    "title": "Timeline",
-                    "subtitle": "Morbi magna orci, consequat in.",
-                    "date": Number(listVideo[0].timeStart)
-                }
-            ]
-        },
-        {
-            "date": 1574125621000,
-            "data": [
-                {
-                    "title": "Beauty Timeline",
-                    "subtitle": "Nulla a eleifend urna. Morbi. Praesent.",
-                    "date": Number(listVideo[0].timeStart)
-                }
-            ]
-        }
-    ];
+   let vlcref = useRef();
+
     useEffect( () => {
-        fetchData();
+        fetchData()
     }, []);
+
     const fetchData = async () => {
         let token = await AsyncStorage.getItem('userToken')
         let myHeaders = new Headers();
@@ -72,13 +40,47 @@ export default function Media(props, route) {
         await fetch(HOST_URL + "camera/savedvideo", requestOptions)
             .then(response => response.json())
             .then(result => {
-                 console.log(result)
                 setListVideo(result || [])
-                setVideo(result[0] || [])
+                setListEvent(formatList(result) || [])
+                setVideo(listEvent[0].data[0] || [])
+
+
             })
             .catch(error => console.log('error', error));
 
     }
+    const formatList = (listVideo) => {
+        let eventVideoArray = [];
+        if (Array.isArray(listVideo) && listVideo.length > 0) {
+            for (let i = 0; i < listVideo.length; i++) {
+                let {timeStart, timeEnd, cdnUrl} = listVideo[i]
+                if (cdnUrl !== null) {
+                    //format data: [{date: '...', data:[{...}]}]
+                    let date = moment(Number(timeStart)).startOf('day')
+                    let indexDate = eventVideoArray.findIndex(item => date.isSame(Number(item.date),'day'));
+
+                    let element = listVideo[i] || {};
+                    element.title= camera && camera.name + 'event' + i;
+                    element.subtitle= 'abc';
+                    element.date= Number(timeStart);
+                    if (indexDate === -1) {
+                        eventVideoArray.push({
+                            date: date,
+                            data: [listVideo[i]]
+                        })
+                    } else {
+
+                        eventVideoArray[indexDate] &&
+                        eventVideoArray[indexDate].data &&
+                        eventVideoArray[indexDate].data.push(listVideo[i])
+                    }
+
+                }
+            }
+        }
+        return eventVideoArray
+    }
+    console.log('dfghjjhgfds',video)
 
     const onPress = (video) => () => {
         const {navigation} = props || {};
@@ -87,23 +89,23 @@ export default function Media(props, route) {
             {video: video}
         )
     }
-const renderVideo=()=>(
-    <VLCPlayer
-        source={{
-            uri: video.url || '',
-        }}
-        style={styles.backgroundVideo}
-        videoAspectRatio="16:9"
-    />
+    const renderVideo = () => (
+        <VLCPlayer
+            // ref={el=> vlcref=el}
+            source={{uri: "https://clientapp.sgp1.digitaloceanspaces.com/5e9471d6cbeb62504f03bc0b/5ed3e22848d6943ed70ec47f/1591588193850.mp4"}}
+            style={styles.backgroundVideo}
+            videoAspectRatio="16:9"
+        />
 
-)
+    )
+
     const renderItem = ({item, index}) => {
-        let {timeStart,timeEnd} = item;
+        let {timeStart, timeEnd} = item;
 
         let start = new Date(Number(timeStart));
         let end = new Date(Number(timeEnd));
         // console.log(timeStart, )
-        return(
+        return (
             <View>
                 {/*<Text>{String(start.getDate())} - {start.getMonth()} - {start.getFullYear()}</Text>*/}
                 {/*<Text>{String(start.getHours())} : {start.getMinutes()} : {start.getSeconds()}</Text>*/}
@@ -121,34 +123,39 @@ const renderVideo=()=>(
             <View><Text>no video</Text></View>
         )
     }
-    const renderOptions=()=>{
-        let {timeStart} = video||{}
-        let start=new Date(Number(timeStart));
-        return(
+    const renderOptions = () => {
+        let {timeStart} = video || {}
+        let start = new Date(Number(timeStart));
+        return (
             <View
-            style={{flexDirection:'row'}}
+                style={{flexDirection: 'row'}}
             >
                 <Text>{String(start.getDate())} - {start.getMonth()} - {start.getFullYear()}</Text>
-                <Button title={'Options'} onPress={()=>{}}/>
+                <Button title={'Options'} onPress={() => {
+                }}/>
                 <Text>{listVideo.length} video</Text>
             </View>
         )
     }
-    if(!listVideo || listVideo.length=== 0) return renderEmpty();
-    else{
-    return (
-        <ScrollView style={styles.container}>
-            {/*<Text>{String(props.route.params)}</Text>*/}
-            {renderVideo()}
-            {renderOptions()}
-            <Timeline data={data} contentContainerStyle={{marginTop:12}}/>
-            {/*<FlatList*/}
-            {/*    data={listVideo}*/}
-            {/*    keyExtractor={(item, index) => index.toString()}*/}
-            {/*    renderItem={renderItem}*/}
-            {/*/>*/}
-        </ScrollView>
-    );
+    if (!listVideo || listVideo.length === 0) return renderEmpty();
+    else {
+        return (
+            <View style={styles.container}>
+                {/*<Text>{String(video)}</Text>*/}
+                {renderVideo()}
+                {renderOptions()}
+                <Timeline
+                    data={listEvent}
+                    contentContainerStyle={{marginTop: 12}}
+                    // isCard={false}
+                />
+                {/*<FlatList*/}
+                {/*    data={listVideo}*/}
+                {/*    keyExtractor={(item, index) => index.toString()}*/}
+                {/*    renderItem={renderItem}*/}
+                {/*/>*/}
+            </View>
+        );
     }
 };
 
@@ -162,7 +169,8 @@ const styles = StyleSheet.create({
     },
     backgroundVideo: {
         height: 200,
-        marginBottom:12
+        marginBottom: 12,
+        backgroundColor: '#000'
     },
 });
 
